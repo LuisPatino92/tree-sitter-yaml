bb068c388cd487264d9bed8fcbf87e69e975de3b
Julian Gomez <ugomez@fluidattacks.com>
2023-03-03 11:51:08 -0500

    refac: modify rules to allow json objects in yml 7

--- src/scanner.cc
+++ src/scanner.cc
@@ -576,10 +576,7 @@ struct Scanner {
         is_lka_wsp = is_wsp(LKA);
         is_lka_saf = (this->*is_plain_safe)(LKA);
 
-        if (cur_chr == ':') {
-          if (is_lka_saf) {MRK_END();UPD_SCH_STT();}
-          else return SCN_FAIL;
-        }
+
       }
     } else return SCN_STOP;
     return SCN_SUCC;
